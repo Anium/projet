9bf64233ac849f5d512affa12bf7f61038e2413b
bastien.rominger.anium <bastien.rominger.anium@gmail.com>
2017-10-14 05:00:49 -0400

    Test

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014063305) do
+ActiveRecord::Schema.define(version: 20171014084645) do
 
   create_table "students", force: true do |t|
     t.string   "name"
     t.date     "birthday"
     t.string   "surname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "towns", force: true do |t|
+    t.string   "Nom"
+    t.string   "Latitude"
+    t.string   "Longitude"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
