ae43d176dc3f8279702fb9d35ab00e114984f566
bastien.rominger.anium <bastien.rominger.anium@gmail.com>
2017-11-23 18:01:53 -0500

    Test 4

--- app/models/town.rb
+++ app/models/town.rb
@@ -4,18 +4,8 @@ class Town < ActiveRecord::Base
   
   private
   def load_position
-    if name.present?
       places = Nominatim.search(name).limit(1)
       self.latitude = places.first.lat
       self.longitude = places.first.lon
-    end
-  end
-
-  def return_forecast
-    ForecastIO.configure do |configuration|
-      configuration.api_key = '2fb4c7915d8b262e68b95907891922a3'
-    end
-     
-    forecast = ForecastIO.forecast(self.latitude,self.longitude)  
   end
 end
